32d93de5ffc4d81922ae1e32a9316dc265584ea6
aswin-13282 <aswin-13282@aswin-13282.csez.zohocorpin.com>
2023-06-21 22:59:15 +0530

    update

--- src/dsa/binaryTree/medium/deepestLeavesSum.ts
+++ src/dsa/binaryTree/medium/deepestLeavesSum.ts
@@ -39,19 +39,28 @@ export const deepestLeavesSum = (root: ITreeNode | null) => {
   return obj[maxDepth] || 0;
 };
 
-let tree = buildBinaryTree([
-  1,
-  2,
-  3,
-  4,
-  5,
-  null,
-  6,
-  7,
-  null,
-  null,
-  null,
-  null,
-  8,
-]);
+export const deepestLeavesSumAlternative = (root: ITreeNode | null) => {
+  if (!root) return null;
+
+  let queue = [root];
+  let sum = 0;
+
+  while (queue.length) {
+    let next = [];
+    sum = 0;
+
+    for (let i = 0; i < queue.length; i++) {
+      let { left, right, val } = queue[i];
+      if (left) next.push(left);
+      if (right) next.push(right);
+      sum += val;
+    }
+
+    queue = next;
+  }
+
+  return sum;
+};
+
+let tree = buildBinaryTree([1, 2, 3, 4, 5, 6, 7, 8]);
 console.log(deepestLeavesSum(tree));
