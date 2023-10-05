0d72553b55d5b77da90b3319e5fedb4a51c71193
aswin-13282 <aswin-13282@aswin-13282.csez.zohocorpin.com>
2023-10-05 18:02:01 +0530

    update

--- src/dsa/string/medium/longestPalindromeByConcatenatingTwoLetterWords.ts
+++ src/dsa/string/medium/longestPalindromeByConcatenatingTwoLetterWords.ts
@@ -16,9 +16,59 @@ Note that "lcyttycl" is another longest palindrome that can be created.
 */
 
 export const longestPalindrome = (words: string[]) => {
+  let pairs = new Map<string, number>();
+  let freq = new Map<string, number>();
   let maxLen = 0;
+  let unpaired = 0;
+
+  for (let word of words) {
+    freq.set(word, (freq.get(word) || 0) + 1);
+  }
+
+  for (let [word, count] of freq) {
+    let reverse = word[1] + word[0];
+
+    if (word === reverse) {
+      let rem = Math.floor(count / 2);
+      if (rem) pairs.set(word, rem);
+      if (count % 2) unpaired++;
+    } else {
+      let min = Math.min(count, freq.get(reverse)!);
+
+      if (!min) continue;
+
+      pairs.set(word, min);
+
+      freq.set(word, count - min);
+      freq.set(reverse, freq.get(reverse)! - min);
+    }
+  }
+
+  for (let [_, count] of pairs) {
+    maxLen += 4 * count;
+  }
+
+  if (unpaired) maxLen += 2;
 
   return maxLen;
 };
 
-console.log(longestPalindrome(["ab", "ty", "yt", "lc", "cl", "ab"]));
+// 26
+console.log(
+  longestPalindrome([
+    "dd",
+    "aa",
+    "bb",
+    "dd",
+    "aa",
+    "dd",
+    "bb",
+    "dd",
+    "aa",
+    "cc",
+    "bb",
+    "cc",
+    "dd",
+    "cc",
+  ])
+);
